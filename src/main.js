f263ec2969715b9719315cabf01db5bb85cc2249
Felix <felix_guerin@hotmail.fr>
2019-07-01 16:09:01 -0400

    Add google fonts link

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import './assets/css/global.scss';
 
 export default function(Vue, { head }) {
 	head.htmlAttrs = { lang: 'fr' };
+	head.link.push({
+		rel: 'stylesheet',
+		href: "https://fonts.googleapis.com/css?family=Alex+Brush&display=swap"
+	});
 }
