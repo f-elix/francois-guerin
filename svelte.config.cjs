e4ab595f0880dcb40740e696e2b749dfc58adba1
f-elix <guerinfelix08@gmail.com>
2021-04-03 00:50:00 -0400

    Remove comments

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -4,8 +4,6 @@ const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		sveltePreprocess({
 			defaults: {
@@ -15,9 +13,6 @@ module.exports = {
 		})
 	],
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
 		adapter: static(),
 		target: '#site',
 		router: false,
