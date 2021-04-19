583bda5ad676daee24021a347714099a5ce15387
Felix Guerin <guerinfelix08@gmail.com>
2021-04-19 19:48:00 -0400

    Update kit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	import Parutions from '$lib/components/Parutions.svelte';
 	import Bio from '$lib/components/Bio.svelte';
