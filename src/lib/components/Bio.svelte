260d490ae8c13e55fc49eaf3a005af466ac2b2eb
Felix Guerin <guerinfelix08@gmail.com>
2021-04-21 21:30:44 -0400

    Add bio pic caption

--- src/lib/components/Bio.svelte
+++ src/lib/components/Bio.svelte
@@ -6,16 +6,19 @@
 
 <Section title="Biographie" id="bio">
 	<article
-		class="flex flex-col items-center xl:items-stretch xl:flex-row space-y-160 xl:space-y-0 xl:space-x-100 max-w-md mx-auto"
+		class="flex flex-col xl:flex-row items-center xl:items-start space-y-160 xl:space-y-0 xl:space-x-100 max-w-md mx-auto"
 	>
-		<picture>
-			<img
-				class="xl:sticky top-80 max-w-320 rounded-10 ring-offset-main ring-black-80 ring-offset-4 ring-4 shadow-2xl"
-				src="{assets}/img/{$site.bio.image}"
-				alt="Portrait de François Guérin"
-				title="François Guérin"
-			/>
-		</picture>
+		<figure class="xl:sticky top-80 space-y-70">
+			<picture>
+				<img
+					class=" max-w-320 rounded-10 ring-offset-main ring-black-80 ring-offset-4 ring-4 shadow-2xl"
+					src="{assets}/img/{$site.bio.image}"
+					alt="Portrait de François Guérin"
+					title="François Guérin"
+				/>
+			</picture>
+			<figcaption class="text-10 text-center">Photo Sophie Perreault</figcaption>
+		</figure>
 		<div class="flex flex-col space-y-70">
 			{#each $site.bio.text as paragraph}
 				<p class="max-w-[70ch]">{paragraph}</p>
