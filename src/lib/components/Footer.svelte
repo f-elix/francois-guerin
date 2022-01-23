123ef977a739cb90568db4c3cfae712929cf737b
Felix Guerin <guerinfelix08@gmail.com>
2022-01-23 16:54:14 -0500

    Remove jcl link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -7,12 +7,6 @@
 <footer
 	class="flex flex-col justify-center items-center space-y-70 _px-security py-100 text-center"
 >
-	<a
-		class="p-50 rounded-10 text-70 _focus-default underline _transition-underline underline-transparent pointer:hover:underline-current"
-		href={footer.jclLink.url}
-		target="_blank"
-		rel="noopener noreferrer">{footer.jclLink.label}</a
-	>
 	<small
 		>&#169; <a
 			class="underline _focus-default"
